6918d63543ebed1eaf5d856508174a2605072661
Andrew Mendez <andrew.mendez@hpe.com>
2023-03-06 13:08:22 -0500

    first commit

---
+++ clone_and_select.sh
@@ -0,0 +1,4 @@
+# git clone --filter=blob:none --no-checkout --depth 1 https://github.com/determined-ai/determined.git 
+# git sparse-checkout --init --clone
+# git sparse-checkout set examples/deepspeed/gpt_neox examples/computer_vision/detectron2_coco_pytorch
+# git checkout main
