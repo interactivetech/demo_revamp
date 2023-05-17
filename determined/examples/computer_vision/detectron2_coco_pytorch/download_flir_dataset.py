9122590be0ac5d1ee2205c6f357c55bbccfc43bd
andrew <inter>
2023-05-17 19:41:44 +0000

    update

--- determined/examples/computer_vision/detectron2_coco_pytorch/download_flir_dataset.py
+++ determined/examples/computer_vision/detectron2_coco_pytorch/download_flir_dataset.py
@@ -2,4 +2,5 @@ from roboflow import Roboflow
 import os
 rf = Roboflow(api_key=os.environ["ROBOFLOW_KEY"])
 project = rf.workspace("roboflow-100").project("flir-camera-objects")
-dataset = project.version(2).download("coco",location="/run/determined/workdir/shared_fs/data/flir-camera-objects")
+# dataset = project.version(2).download("coco",location="/run/determined/workdir/shared_fs/data/flir-camera-objects")
+dataset = project.version(2).download("yolov8",location="/run/determined/workdir/shared_fs/data/flir-camera-objects-yolo")
